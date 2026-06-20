7aa560fe13a86bc5c023914a2f1a231a23b12674
tayyabali77865-tech <tayyabali77865@gmail.com>
2026-06-20 22:40:42 +0500

    Update CSP to allow ArtPlayer CDN and related stylesheets

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ module.exports = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; media-src 'self' https: blob:; connect-src 'self' https:; frame-src 'self' https:; frame-ancestors 'self';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://spedostream2.shop; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data: https:; media-src 'self' https: blob:; connect-src 'self' https:; frame-src 'self' https:; frame-ancestors 'self';",
           }
         ],
       },
