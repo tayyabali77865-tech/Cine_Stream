85641a6dd03be13c75ed91eeda41abe6c987b2ae
tayyabali77865-tech <tayyabali77865@gmail.com>
2026-06-22 04:46:21 +0500

    install @vercel/analytics and integrate component and static tracking script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
